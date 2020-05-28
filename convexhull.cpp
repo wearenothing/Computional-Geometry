37edcd60ca4e8d445841f5a447096f721940fb99
wearenothing <39990563+wearenothing@users.noreply.github.com>
2020-05-28 08:11:43 +0800

    Update convexhull.cpp

--- convexhull.cpp
+++ convexhull.cpp
@@ -1,110 +1,43 @@
 #include<iostream>
-#include<vector>
-#include<algorithm> 
 using namespace std;
-//Point 类 
 class Point
 {
 	public:
-		Point(int x,int y)
+		Point(int xx,int yy)
 		{
-			this->x=x;
-			this->y=y;	
+			x=xx;
+			y=yy;	
 		};
 		Point()
 		{
-			this->x=0;
-			this->y=0;
+			x=0;
+			y=0;
 		} 
-		int getX() const{
-		return x;
-		}
-		int getY() const{
-		return y;
-		}
-		int id;
-	private: 
-	
-	int x,y;
+		int x,y;
 } ;
-//比较Point 的大小 
-bool cmp_point(Point a,Point b)
+int TLT(Point s[],int n)
 {
-	if(a.getX()!=b.getX())
-	return a.getX()<b.getX();
-	else
-	return a.getY()<b.getY();
-};
-//判断r点在线pq右边的函数,在pq上也算 
-bool isRightTurn(Point p,Point q,Point r)
+	int ltl=0;
+	for(int i=1;i<n;i++)
+	{
+		if(s[i].x<s[ltl].x||(s[i].x==s[ltl].x&&s[i].y<s[ltl].y))
+		ltl=i;
+	}
+	return ltl;
+}
+bool ToLeft(Point a,Point b,Point c)//if Point c locate on the left of line ab
 {
-	int area2= p.getX()*q.getY()+r.getX()*p.getY()+q.getX()*r.getY()-q.getY()*r.getX()-p.getX()*r.getY()-q.getX()*p.getY();
-	if(area2<=0)
+	int area=a.x*b.y+c.x*a.y+b.x*c.y-c.x*b.y-a.x*c.y-b.x*a.y;
+	if area>0// ignore degeneracy 
 	return true;
-	else
+	else 
 	return false;
+}
+bool compare(Point a, Point b) 
+{
+	if(ToLeft())
 }
 int main()
 {
-	//Input
-	int n;
-	cin>>n;
-	vector<Point> P(n);
-	for(int i=0;i<n;i++) 
-	{
-		int x,y;
-		cin>>x>>y;
-		P[i]=Point(x,y);
-		P[i].id=i+1;
-	}
-	
-	//Sorting
-	sort(P.begin(),P.end(),cmp_point);
-	
-	//upper hull
-	vector<int> up_hull(2);
-	up_hull[0]=0;
-	up_hull[1]=1;
-
-	for(int i=2;i<n;i++)
-	{
-		up_hull.push_back(i);
-		//取uphull最后面三个点 
-		while(up_hull.size()>=3&&!isRightTurn(P[up_hull[up_hull.size()-3]],P[up_hull[up_hull.size()-2]],P[up_hull[up_hull.size()-1]]))
-		{
-			vector<int>::iterator iter=up_hull.end();
-			up_hull.erase(iter-2); //输出倒数第二个元素 	
-		}
-	}
-	/*for(int i=0;i<up_hull.size();i++)
-		cout<<P[up_hull[i]].id<<" ";
-	cout<<endl;*/
-	//lower hull
-	
-	vector<int> low_hull(2);
-	low_hull[0]=n-1;
-	low_hull[1]=n-2;
-
-	for(int i=n-3;i>=0;i--)
-	{
-		low_hull.push_back(i);
-		//取uphull最后面三个点 
-		while(low_hull.size()>=3&&!isRightTurn(P[low_hull[low_hull.size()-3]],P[low_hull[low_hull.size()-2]],P[low_hull[low_hull.size()-1]]))
-		{
-			vector<int>::iterator iter=low_hull.end();
-			low_hull.erase(iter-2); //输出倒数第二个元素 	
-		}
-	}
-	/*for(int i=0;i<low_hull.size();i++)
-			cout<<P[low_hull[i]].id<<" ";
-			cout<<endl;*/
-	int h=low_hull.size()+up_hull.size()-2;
-	long mult1=1;
-	long mult2=1;
-	for(int i=0;i<up_hull.size();i++)
-		mult1*=P[up_hull[i]].id;
-	for(int i=1;i<low_hull.size()-1;i++)
-			mult2*=P[low_hull[i]].id;
-	cout<<(mult1*mult2*h)%(n+1);
-	return 0;
+return 0;
 }
